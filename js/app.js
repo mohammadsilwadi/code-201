db0cef618bcdc7ca0fc7ad559d8033f86b6c3d7b
mohammadsilwadi <mohammadsilwadi1@gmail.com>
2021-06-06 15:42:29 +0300

    html file+jave file +css file

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let userName = prompt('whats your name?');    
+let userAge = prompt('who old are ?');    
+let location = prompt('where you live ?');
+let userTeam = prompt('which club you support?!'); 
+alert(userName+ 'how old '+userAge+ 'live in '+ location+' and support '+ userTeam);
+console.log(userName+userAge+location+userTeam)
