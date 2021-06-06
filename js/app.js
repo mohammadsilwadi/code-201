e963547d501d1ebfc766dc116fd32bc5cc1bf8a4
mohammadsilwadi <mohammadsilwadi1@gmail.com>
2021-06-06 16:01:41 +0300

    new change

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
-let userName = prompt('whats your name?');    
-let userAge = prompt('who old are ?');    
-let location = prompt('where you live ?');
-let userTeam = prompt('which club you support?!'); 
-alert(userName+ 'how old '+userAge+ 'live in '+ location+' and support '+ userTeam);
-console.log(userName+userAge+location+userTeam)
+let userName = prompt('Please enter you name!');    
+ let userAge = prompt('who old are ?');    
+ let place = prompt('where you live ?');;
+ let userTeam = prompt('which club you support?!');; 
+alert( userName + ' how old '+ userAge + ' live in '+ place +' and support '+ userTeam );
+console.log(userName);
+console.log(userAge);
+console.log(place);
+console.log(userTeam);
+// alert('Welcome ' + userName + ' to our website');
+
+//alert(`You name is ${userName}, welcome to our website`);
+//console.log(`You name is ${userName}, welcome to our website`);
